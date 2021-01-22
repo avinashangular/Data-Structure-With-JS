608f00d2e1b87d3eefa2cffa7857ebbffd9e1ced
avinashangular <avinash.angular@gmail.com>
2021-01-22 10:36:08 +0530

    implemented shift and unshift method

--- src/array.js
+++ src/array.js
@@ -9,10 +9,10 @@ class Array {
    * @author Avinash Kumar
    */
   constructor(arr) {
-      if(arr){
-          this.#arr = arr
-          this.#indexCounter = arr.length
-      }
+    if (arr) {
+      this.#arr = arr;
+      this.#indexCounter = arr.length;
+    }
   }
 
   /**
@@ -64,6 +64,38 @@ class Array {
    * @author Avinash Kumar
    */
   length() {
+    return this.#indexCounter;
+  }
+
+  /**
+   * Used to delete first element of an Array.
+   * @returns {element} Returns deleted element
+   * @author Avinash Kumar
+   */
+  shift() {
+    let temp = [];
+    for (let i = 1; i < this.#indexCounter; i++) {
+      temp.push(this.#arr[i]);
+    }
+    this.#indexCounter -= 1;
+    let deletedValue = this.#arr[0];
+    this.#arr = temp;
+    return deletedValue;
+  }
+
+  /**
+   * Used to add an element at start of an Array.
+   * @param {any} element Element to be added.
+   * @returns {Array} Return new array length
+   * @author Avinash Kumar
+   */
+  unshift(element) {
+    let tempArray = [element];
+    for (let i = 0; i < this.#indexCounter; i++) {
+      tempArray.push(this[i]);
+    }
+    this.#indexCounter++;
+    this.#arr = tempArray;
     return this.#indexCounter;
   }
 }
