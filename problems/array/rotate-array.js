2390ca4eca6fbb031e1f7a2c0d650bd5d94b20a3
avinashangular <avinash.angular@gmail.com>
2021-01-22 10:37:41 +0530

    impelemented second method of rotate array

--- problems/array/rotate-array.js
+++ problems/array/rotate-array.js
@@ -1,8 +1,15 @@
 import { Array } from '../../src/array.js';
 
-function RotateArray(arr, d){
+/**
+ * Rotate Array element by using temp array;
+ * @param {Array} arr Source array.
+ * @param {number} d Number of element thar is need to be rotate.
+ * @returns {Array} Returns New Array
+ * @author Avinash Kumar
+ */
+function RotateArray_method_one(arr, d){
     let arry = new Array(arr);
-    
+
     let temp = new Array();
     for(let i = d; i < arry.length(); i++){        
         temp.push(arry.getElementByIndex(i));
@@ -14,7 +21,25 @@ function RotateArray(arr, d){
     return temp;
 }
 
-let result = RotateArray([10,20,30,40,50],2);
-console.log(result);
 
-export { RotateArray };
+/**
+ * Rotate Array element by using temp variable;
+ * @param {Array} arr Source array.
+ * @param {number} d Number of element thar is need to be rotate.
+ * @returns {Array} Returns New Array
+ * @author Avinash Kumar
+ */
+function RotateArray_method_two(arr, d){
+    let arry = new Array(arr);
+
+    for(let i = 0; i < d; i++){
+        arry.push(arry.shift());
+    }
+
+    return arry;
+}
+
+let rotatedArray = RotateArray_method_two([10,20,30,40,50],2);
+console.log(rotatedArray);
+
+export { RotateArray_method_one, RotateArray_method_two};
